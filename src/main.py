efab3a3d5d1e22184ecca907cfd65ad6e5370bd4
Carlos Morales <71106584+carlosmgnc@users.noreply.github.com>
2024-12-02 17:16:54 -0800

    added tof line search and updated figures

--- src/main.py
+++ src/main.py
@@ -1,132 +1,162 @@
 import numpy as np
 import cvxpy as cvx
-import pandas as pd
 import scipy as sp
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
 from matplotlib.animation import FuncAnimation
 
-#vehicle properties
-g = -3.7114
-mw = 1905
-md = 1505
+class opt_problem:
+  def __init__(self):
+    #vehicle and dynamics properties
+    self.g = -3.7114
+    self.mw = 1905
+    self.md = 1505
+    self.Isp = 225
+    self.T = 3100
+    self.T1 = 0.3*self.T
+    self.T2 = 0.8*self.T
+    self.phi = np.deg2rad(27)
+    self.alpha = 1 / (self.Isp*9.81*np.cos(self.phi))
+    self.nthrust = 6
+    self.rho1 = self.nthrust * self.T1 * np.cos(self.phi)
+    self.rho2 = self.nthrust * self.T2 * np.cos(self.phi)
+    self.z_init = np.log(self.mw)
 
-# normal init conditions
-r0 = np.array([[5000], [3000], [2000]])
-rdot0 = np.array([[-110], [-100], [100]])
+    # initial state
+    self.r0 = np.array([[5000], [3000], [2000]])
+    self.rdot0 = np.array([[-110], [-100], [100]])
+    self.y0 = np.vstack([self.r0, self.rdot0, self.z_init])
 
-#other init conditions
-# r0 = np.array([[5000], [100], [5000]])
-# rdot0 = np.array([[-50], [-150], [-150]])
-# rdot0 = np.array([[0], [0], [0]])
+    self.dt = 1
+    self.tmin = (self.md)*np.linalg.norm(self.rdot0)/self.rho2
+    self.tmax = (self.mw-self.md)/(self.alpha*self.rho1)
 
-z_init = np.log(mw)
-y0 = np.vstack([r0, rdot0, z_init])
+    self.Ntmin = int(self.tmin/self.dt) + 1
+    self.Ntmax = int(self.tmax/self.dt)
 
-Isp = 225
-T = 3100
-T1 = 0.3*T
-T2 = 0.8*T
-phi = np.deg2rad(27)
-alpha = 1 / (Isp*9.81*np.cos(phi))
-nthrust = 6
-rho1 = nthrust * T1 * np.cos(phi)
-rho2 = nthrust * T2 * np.cos(phi)
+    self.cost_list = []
+    self.Nt_list = []
 
-dt = 1
-Nt = 80
-time = np.arange(0, Nt, dt)
+    #continuouss time dynamics Ac (7x7), Bc (7x4)
+    Ac = np.block([[np.zeros((3, 3)), np.eye(3), np.zeros((3, 1))], [np.zeros((4, 7))]])
+    Bc = np.block([[np.zeros((3, 4))], [np.eye(3), np.zeros((3, 1))], [np.zeros((1, 3)), -self.alpha]])
 
-#continues time dynamics Ac (7x7), Bc (7x4)
-Ac = np.block([[np.zeros((3, 3)), np.eye(3), np.zeros((3, 1))], [np.zeros((4, 7))]])
-Bc = np.block([[np.zeros((3, 4))], [np.eye(3), np.zeros((3, 1))], [np.zeros((1, 3)), -alpha]])
+    #discretize by computing state transition matrix for 0->dt
+    block_n = Ac.shape[1]+Bc.shape[1]
+    exp_aug = sp.linalg.expm(self.dt*np.block([[Ac, Bc], [np.zeros((block_n-Bc.shape[0], block_n))]]))
 
-#discretize by computing state transition matrix for 0->dt
-block_n = Ac.shape[1]+Bc.shape[1]
-exp_aug = sp.linalg.expm(dt*np.block([[Ac, Bc], [np.zeros((block_n-Bc.shape[0], block_n))]]))
+    self.A = exp_aug[:Ac.shape[0], :Ac.shape[1]]
+    self.B = exp_aug[:Ac.shape[0], Ac.shape[1]:]
 
-A = exp_aug[:Ac.shape[0], :Ac.shape[1]]
-B = exp_aug[:Ac.shape[0], Ac.shape[1]:]
+    eigs, eigvecs = np.linalg.eig(self.A)
 
-eigs, eigvecs = np.linalg.eig(A)
+    #variable "extraction" matrices
+    E = np.hstack([np.eye(6), np.zeros((6, 1))])
+    F = np.hstack([np.zeros((1, 6)), np.array([[1]])])
+    Eu = np.hstack([np.eye(3), np.zeros((3, 1))])
 
-#pre calculate all A^k and A^(k-1)B
-Ak_list = np.array([np.linalg.matrix_power(A, k) for k in range(0, Nt+1)])
-AB_list = np.array([Ak_list[k, ::] @ B for k in range(0,Nt)])
+  ############################# cvx problem #############################
 
-#variable "extraction" matrices
-E = np.hstack([np.eye(6), np.zeros((6, 1))])
-F = np.hstack([np.zeros((1, 6)), np.array([[1]])])
-Eu = np.hstack([np.eye(3), np.zeros((3, 1))])
+  def solve_cvx_problem(self, Nt):
+    time_Nt = np.arange(0, Nt*self.dt, self.dt)
 
-#SOCP optimization problem formulation for CVXPY
-y = cvx.Variable((7, Nt))
-p = cvx.Variable((4, Nt-1))
+    y = cvx.Variable((7, Nt))
+    p = cvx.Variable((4, Nt-1))
 
-cost = 0
-constraints = []
-constraints += [y[:, [0]] == y0]
+    cost = 0
+    constraints = []
+    constraints += [y[:, [0]] == self.y0]
 
-#stay above ground
-constraints += [y[0, :] >= 0]
+    #stay above ground constraint
+    constraints += [y[0, :] >= 0]
 
-#hard final state constraint
-constraints += [y[:6, [-1]] == np.array([[0], [0], [0], [0], [0], [0]])]
+    #hard final state constraint
+    constraints += [y[:6, [-1]] == np.array([[0], [0], [0], [0], [0], [0]])]
 
-#gravity input
-g_input = np.vstack([g, np.zeros((3, 1))])
+    #gravity input vector
+    g_input = np.vstack([self.g, np.zeros((3, 1))])
 
-# #wind disturbance
-# wind_input = np.vstack(0, wind, np.zeros((3, 1))])
+    for k in range(Nt-1):
+      #dynamics constraints
+      constraints += [y[:, [k+1]] == self.A @ y[:,[k]] + self.B @ (p[:,[k]] + g_input)]
+        
+      #relaxed thrust constraints
+      constraints += [cvx.norm(p[:3, [k]]) <= p[3, k]]
 
-for k in range(Nt-1):
-  #dynamics constraints
-  constraints += [y[:, [k+1]] == A @ y[:,[k]] + B @ (p[:,[k]] + g_input)]
+      z = y[6,k]
+      z0 = cvx.log(self.mw - self.alpha*self.rho2*time_Nt[k])
+      mu1 = self.rho1*cvx.exp(-z0)
+      mu2 = self.rho2*cvx.exp(-z0)
+
+      constraints += [mu1*(1 - (z-z0) + cvx.power(z-z0, 2)*0.5) <= p[3, k]]
+      constraints += [p[3, k] <= mu2*(1-(z-z0))]
+
+      constraints += [z0 <= y[6, k]]
+      constraints += [y[6, k] <= cvx.log(self.mw - self.alpha*self.rho1*time_Nt[k])]
+
+      cost += p[3, k] * self.dt
+
+    # #final state cost (relaxation on hard terminal constraints)
+    # R = np.diag([10, 10, 10, 100, 10, 10 ])
+    # cost += cvx.quad_form(y[:6, [-1]], R)
+
+    objective = cvx.Minimize(cost)
+    prob = cvx.Problem(objective, constraints)
+    prob.solve()
+    print("solver status: " + prob.status)
+
+    if prob.status != 'optimal':
+      opt_cost = float('inf')
+      return opt_cost, None, None
     
-  #convexified thrust constraints
-  constraints += [cvx.norm(p[:3, [k]]) <= p[3, k]]
-  if k > 0:
-    #variables
-    z = y[6,k]
-    z0 = cvx.log(mw - alpha*rho2*time[k])
-    mu1 = rho1*cvx.exp(-z0)
-    mu2 = rho2*cvx.exp(-z0)
+    else:
+      opt_cost = cost.value
+      traj_Nt = y.value
+      u_Nt = p.value
+      return opt_cost, traj_Nt, u_Nt
 
-    #convexified thrust constraint
-    constraints += [mu1*(1 - (z-z0) + cvx.power(z-z0, 2)*0.5) <= p[3, k]]
-    constraints += [p[3, k] <= mu2*(1-(z-z0))]
+  #line search using golden section search
+  def opt_min_time(self):
+    Nt_search = np.arange(self.Ntmin, self.Ntmax, 1)
+    g = 2/(1+np.sqrt(5))
+    a = 0
+    b = len(Nt_search)
 
-    #mass constraints
-    constraints += [z0 <= y[6, k]]
-    constraints += [y[6, k] <= cvx.log(mw - alpha*rho1*time[k])]
+    while abs(b - a) > 1:
+      c = b - int(np.floor((b - a) * g))
+      d = a + int(np.ceil((b - a) * g))
+      fc, trajc, uc = self.solve_cvx_problem(Nt_search[c])
+      fd, _, _ = self.solve_cvx_problem(Nt_search[d])
 
-  cost += p[3, k] * dt
+      self.cost_list.extend([fc, fd])
+      self.Nt_list.extend([Nt_search[c], Nt_search[d]])
 
-# #final state cost (relaxation on hard terminal constraints)
-# R = np.diag([10, 10, 10, 100, 10, 10 ])
-# cost += cvx.quad_form(y[:6, [-1]], R)
+      if fc < fd:
+          b = d
+      else:
+          a = c
+    
+    return Nt_search[c], trajc, uc
 
-objective = cvx.Minimize(cost)
-prob = cvx.Problem(objective, constraints)
-opt_val = prob.solve()
-solution = y.value
-print("solver status: " + prob.status)
-print("Final state: ", str(y[:6, -1].value))
+opt = opt_problem()
+Nt_opt, traj, u = opt.opt_min_time()
+time = np.arange(0, Nt_opt*opt.dt, opt.dt)
 
 ############################# plotting #############################
 
 #pos vs time plot
 plt.figure(1)
 plt.title("pos vs time")
+labels = []
 for i in range(3):
-  plt.plot(time, y[i, :].value)
-
-traj = y.value
-u = p.value
+  plt.plot(time, traj[i, :], label='')
+plt.legend(['x', 'y', 'z'])
+plt.xlabel("time (s)")
+plt.ylabel("position (m)")
 
 #plot thrust vector
-qlen = 100
-q = qlen*u[:3, :]
+qlen = 0.05
+thrust_vecs = u[:3, :]*np.exp(traj[6, :-1])
+q = qlen*thrust_vecs
 base_x = traj[0, :-1] - q[0, :]
 base_y = traj[1, :-1] - q[1, :]
 base_z = traj[2, :-1] - q[2, :]
@@ -168,27 +198,42 @@ def shared_traj_plot_properties(ax):
 
 shared_traj_plot_properties(ax)
 
-#control effort plot
+#control effort plots
 plt.figure(3)
-plt.title("control effort (thrust/m) vs time")
-axis_list = ["x", "y", "z"]
-for i in range(p.shape[0]-1):
-  plt.plot(time[:-1], p[i, :].value, label="(T/m)_" + axis_list[i])
-  plt.legend()
+plt.title("norm(thrust_vector) vs time")
+plt.plot(time[:-1], np.linalg.norm(thrust_vecs[:, :], axis=0))
+plt.xlabel("time (s)")
+plt.ylabel("thrust (N)")
 
 plt.figure(4)
-plt.title("mass vs time")
-plt.plot(time[:], np.exp(y[6, :].value))
+plt.title("thrust vector components vs time")         
+for i in range(3):
+  plt.plot(time[:-1], thrust_vecs[i, :])
+plt.legend(['T_x', 'T_y', 'T_z'])
+plt.xlabel("time (s)")
+plt.ylabel("thrust (N)")
 
-lower_bound = [mw - alpha*rho2*time[k] for k in range(Nt)]
-upper_bound = [mw - alpha*rho1*time[k] for k in range(Nt)]
+plt.figure(5)
+plt.title("mass vs time")
+plt.plot(time[:], np.exp(traj[6, :]))
+
+lower_bound = [opt.mw - opt.alpha*opt.rho2*time[k] for k in range(Nt_opt)]
+upper_bound = [opt.mw - opt.alpha*opt.rho1*time[k] for k in range(Nt_opt)]
 plt.plot(time[:], lower_bound[:])
 plt.plot(time[:], upper_bound[:])
 plt.legend(["m(t)", "lowerbound", "upperbound"])
+plt.xlabel("time (s)")
+plt.ylabel("mass (kg)")
+
+plt.figure(6)
+plt.title("cost vs time of flight (Nt timesteps of 1 sec)")
+plt.scatter(opt.Nt_list, opt.cost_list)
+plt.xlabel("Nt")
+plt.ylabel("cost (m/s)")
 
 ############################# animation #############################
-fig_anim = plt.figure(5, figsize=(8, 8))
 
+fig_anim = plt.figure(7, figsize=(8, 8))
 
 ax_anim = plt.axes(projection = '3d')
 ax_anim.view_init(elev=15, azim=-160)
@@ -210,8 +255,13 @@ def update(frame):
   return pos_scatter, quiver, 
 
 anim_int = 50
-animation = FuncAnimation(fig_anim, update, frames= Nt-1, interval= anim_int)
-#animation.save('../images/animation.gif', writer='imagemagick', fps=1000/anim_int)
+animation = FuncAnimation(fig_anim, update, frames= Nt_opt-1, interval= anim_int)
+
+fig_names = ['position', 'trajectory', 'throttle', 'thrusts', 'mass', 'cost_tof']
+for i in range(1, 7):
+  plt.figure(i).savefig("../images/" + fig_names[i-1] +  ".pdf")
+
+animation.save('../images/animation.gif', writer='pillow', fps=1000/anim_int)
 
 #show plots, wait for input to close
 plt.show(block=False)
